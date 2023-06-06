faaee1e079d4032a8dcf6eb9d668ff94eef09eca
Felipe Ferreira <flpferreira1999@gmail.com>
2023-06-05 21:56:59 -0300

    Sistema de Previsão do tempo usando API

--- README.md
+++
@@ -1,2 +0,0 @@
-# Sistema-Previsao-Tempo
-Sistema de previsão de tempo usando javascript, css e html. É usado uma API para pegar a previsão em tempo real
---
+++ scripts.js
@@ -0,0 +1,27 @@
+const key = "67d11c6dfcc55f86b8fb253fca35d3f9"
+
+
+
+function colocarDadosNaTela(dados) {
+
+    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
+    document.querySelector(".temperatura").innerHTML = Math.floor(dados.main.temp) + "°C"
+    document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description
+    document.querySelector(".umidade").innerHTML = "Umidade " + dados.main.humidity + "%"
+    document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+}
+
+
+async function buscarCidade(cidade) {
+
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric `).then(response => response.json()) // await faz com que espere a resposta da api
+    
+    colocarDadosNaTela(dados);
+}
+
+
+function procurarCidade(){
+    const cidade = document.querySelector(".input-cidade").value;
+
+    buscarCidade(cidade);
+}
